eddea397dffbf29753ef528958148315c16d5bb7
Sourav Mondal Shuvo <souravmondal033@gmail.com>
2026-02-13 23:21:59 +0600

    CSE220 Spring 2026 Lab Assignment 1 'Assignment Task 02: Matrix Compression'

--- CSE220 Spring 2026 Lab Assignments 1/AssgnTask2.java
+++ CSE220 Spring 2026 Lab Assignments 1/AssgnTask2.java
@@ -9,8 +9,29 @@ class AssgnTask2{
         
         //TO DO
 
+        int row = matrix.length;
+        int column = matrix[0].length;
+
+        int new_row = row / 2;
+        int new_column = column / 2;
+
+        Integer[][] compressed_2D_matrix = new Integer[new_row][new_column];
+
+        for(int i = 0; i < new_row; i++) {
+            for(int j = 0; j < new_column; j++) {
+
+                int matrix_row = 2 * i;
+                int matrix_column = 2 * j;
+
+                int summed_up_matrix = matrix[matrix_row][matrix_column] + matrix[matrix_row][matrix_column + 1] + matrix[matrix_row + 1][matrix_column] + matrix[matrix_row + 1][matrix_column + 1];
+
+                compressed_2D_matrix[i][j] = summed_up_matrix;
+            }
+        }
+
         //remove the line below and return the newly created Compressed 2D matrix
-        return null;
+        // return null;
+        return compressed_2D_matrix;
     }
 
     //DO NOT CHANGE ANY DRIVER CODE BELOW THIS LINE
