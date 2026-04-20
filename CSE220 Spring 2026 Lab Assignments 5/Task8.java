e2131ebcb5e82c3d99c810ebfc32594b04c4051d
Sourav Mondal Shuvo <souravmondal033@gmail.com>
2026-04-20 06:17:41 +0600

    TASK#8 completed

--- CSE220 Spring 2026 Lab Assignments 5/Task8.java
+++ CSE220 Spring 2026 Lab Assignments 5/Task8.java
@@ -12,6 +12,19 @@ public class Task8 {
     // You can use extra helper private static methods as per need
     public static void swapChild( BTNode root, int lvl, int M ){
         //TO DO
+
+        if(root == null) {
+            return;
+        }
+
+        if(lvl < M) {
+            BTNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+        }
+
+        swapChild(root.left, lvl + 1, M);
+        swapChild(root.right, lvl + 1, M);
     }
     //============================================================================
 }
