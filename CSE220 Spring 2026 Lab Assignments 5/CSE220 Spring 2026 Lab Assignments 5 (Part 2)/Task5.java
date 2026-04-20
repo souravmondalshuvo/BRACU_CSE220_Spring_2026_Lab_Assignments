fb00615e696772c55c2f81df77643462a0392b7a
Sourav Mondal Shuvo <souravmondal033@gmail.com>
2026-04-21 00:42:22 +0600

    Lab 5 Part 2 TASK#5 completed

--- CSE220 Spring 2026 Lab Assignments 5/CSE220 Spring 2026 Lab Assignments 5 (Part 2)/Task5.java
+++ CSE220 Spring 2026 Lab Assignments 5/CSE220 Spring 2026 Lab Assignments 5 (Part 2)/Task5.java
@@ -8,7 +8,22 @@ public class Task5 {
     // You can use extra helper private static methods as per need
     public static Integer mirrorSum( BSTNode root ){
         //TO DO
-        return null; // remove this line
+
+        if(root == null) {
+            return 0;
+        }
+
+        return mirrorSumHelper(root.left, root.right);
+        // return null; // remove this line
+    }
+
+    public static Integer mirrorSumHelper(BSTNode left_node, BSTNode right_node) {
+        
+        if(left_node == null || right_node == null) {
+            return 0;
+        }
+
+        return left_node.elem + right_node.elem + mirrorSumHelper(left_node.left, right_node.right) + mirrorSumHelper(left_node.right, right_node.left);
     }
     //===============================================================
 
