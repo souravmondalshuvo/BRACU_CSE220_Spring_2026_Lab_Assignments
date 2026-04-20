ec5734d2bdb7e9187dda128c8901b48b1ad01866
Sourav Mondal Shuvo <souravmondal033@gmail.com>
2026-04-21 00:59:44 +0600

    TASK#6 completed

--- CSE220 Spring 2026 Lab Assignments 5/CSE220 Spring 2026 Lab Assignments 5 (Part 2)/Task6.java
+++ CSE220 Spring 2026 Lab Assignments 5/CSE220 Spring 2026 Lab Assignments 5 (Part 2)/Task6.java
@@ -8,7 +8,22 @@ public class Task6 {
     // You can use extra helper private static methods as per need
     public static Boolean isBST( BSTNode root ){
         //TO DO
-        return null; // remove this line
+
+        return isBSTHelper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        // return null; // remove this line
+    }
+
+    private static Boolean isBSTHelper(BSTNode root, long min, long max) {
+        
+        if(root == null) {
+            return true;
+        }
+
+        if(root.elem <= min || root.elem >= max) {
+            return false;
+        }
+
+        return isBSTHelper(root.left, min, root.elem) && isBSTHelper(root.right, root.elem, max);
     }
     //===============================================================
 
