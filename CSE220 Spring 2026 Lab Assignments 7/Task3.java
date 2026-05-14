e3e298374039133fcf2e53d3931a262c7181e279
souravmondalshuvo <191050332+souravmondalshuvo@users.noreply.github.com>
2026-05-15 03:05:35 +0600

    Task3a of Task3 completed

--- CSE220 Spring 2026 Lab Assignments 7/Task3.java
+++ CSE220 Spring 2026 Lab Assignments 7/Task3.java
@@ -24,4 +24,25 @@ public class Task3 {
 
         return new int[] {maxVertex, maxOutDegree};
     }
+
+    public static int[] Task3a_MaxOutgoingWeightSum(int[][] matrix) {
+
+        int maxVertex = -1;
+        int maxWeightSum = -1;
+
+        for(int i = 0; i < matrix.length; i++) {
+            int weightSum = 0;
+
+            for(int j = 0; j < matrix[i].length; j++) {
+                weightSum += matrix[i][j];
+            }
+
+            if(weightSum > maxWeightSum) {
+                maxWeightSum = weightSum;
+                maxVertex = i;
+            }
+        }
+
+        return new int[] {maxVertex, maxWeightSum};
+    }
 }
