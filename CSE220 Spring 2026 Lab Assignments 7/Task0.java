2222cb3cb07dd8e9d7bcea63c6da7a61c645dc2a
souravmondalshuvo <191050332+souravmondalshuvo@users.noreply.github.com>
2026-05-15 01:43:44 +0600

    Task0b of Task0 completed

--- CSE220 Spring 2026 Lab Assignments 7/Task0.java
+++ CSE220 Spring 2026 Lab Assignments 7/Task0.java
@@ -23,4 +23,33 @@ public class Task0 {
             System.out.println();
         }
     }
+
+    // ========================== Task 0b ==========================    
+
+    public static void addDirectedListEdge(EdgeNode[] list, int from, int to, int weight) {
+
+        list[from] = new EdgeNode(to, weight, list[from]);
+    }
+
+    public static void addUndirectedListEdge(EdgeNode[] list, int u, int v, int weight) {
+
+        addDirectedListEdge(list, u, v, weight);
+        addDirectedListEdge(list, v, u, weight);
+    }
+
+    public static void printList(EdgeNode[] list) {
+
+        for(int i = 0; i < list.length; i++) {
+            System.out.print((i + 1) + " -> ");
+
+            EdgeNode current = list[i];
+
+            while(current != null) {
+                System.out.println("(" + (current.to + 1) + ", w=" + current.weight + ") ");
+                current = current.next;
+            }
+
+            System.out.println();
+        }
+    }
 }
