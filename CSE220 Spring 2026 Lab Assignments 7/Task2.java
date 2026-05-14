cfcdd83694610ec992fd2f02d789d45c9d19bb0f
souravmondalshuvo <191050332+souravmondalshuvo@users.noreply.github.com>
2026-05-15 02:40:43 +0600

    Task2b of Task2 completed

--- CSE220 Spring 2026 Lab Assignments 7/Task2.java
+++ CSE220 Spring 2026 Lab Assignments 7/Task2.java
@@ -22,4 +22,30 @@ public class Task2 {
 
         return new int[] {maxVertex, maxWeightSum};
     }
+
+    // ========================== Task 2a ==========================
+    
+    public static int[] Task2b (EdgeNode[] list) {
+
+        int maxVertex = -1;
+        int maxWeightSum = -1;
+
+        for(int i = 0; i < list.length; i++) {
+            int weightSum = 0;
+
+            EdgeNode current = list[i];
+
+            while(current != null) {
+                weightSum += current.weight;
+                current = current.next;
+            }
+
+            if(weightSum > maxWeightSum) {
+                maxWeightSum = weightSum;
+                maxVertex = i;
+            }
+        }
+
+        return new int[] {maxVertex, maxWeightSum};
+    }
 }
