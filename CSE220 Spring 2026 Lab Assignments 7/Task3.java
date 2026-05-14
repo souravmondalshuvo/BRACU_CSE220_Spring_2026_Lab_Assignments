13942fbd4a9070cfe538129a5838e5eb9b1e7fbf
souravmondalshuvo <191050332+souravmondalshuvo@users.noreply.github.com>
2026-05-15 03:13:21 +0600

    Task3b of Task3 completed

--- CSE220 Spring 2026 Lab Assignments 7/Task3.java
+++ CSE220 Spring 2026 Lab Assignments 7/Task3.java
@@ -45,4 +45,55 @@ public class Task3 {
 
         return new int[] {maxVertex, maxWeightSum};
     }
+
+    // ========================== Task 3b ==========================
+
+    public static int[] Task3b_MaxOutgoingDegree(EdgeNode[] list) {
+
+        int maxVertex = -1;
+        int maxOutDegree = -1;
+
+        for(int i = 0; i < list.length; i++) {
+            int outDegree = 0;
+
+            EdgeNode current = list[i];
+
+            while(current != null) {
+                outDegree++;
+                current = current.next;
+            }
+
+            if(outDegree > maxOutDegree) {
+                maxOutDegree = outDegree;
+                maxVertex = i;
+            }
+        }
+
+        return new int[] {maxVertex, maxOutDegree};
+    }
+
+    public static int[] Task3b_MaxOutgoingWeightSum(EdgeNode[] list) {
+
+
+        int maxVertex = -1;
+        int maxWeightSum = -1;
+
+        for(int i = 0; i < list.length; i++) {
+            int weightSum = 0;
+            
+            EdgeNode current = list[i];
+
+            while(current != null) {
+                weightSum += current.weight;
+                current = current.next;
+            }
+
+            if(weightSum > maxWeightSum) {
+                maxWeightSum = weightSum;
+                maxVertex = i;
+            }
+        }
+
+        return new int[] {maxVertex, maxWeightSum};
+    }
 }
