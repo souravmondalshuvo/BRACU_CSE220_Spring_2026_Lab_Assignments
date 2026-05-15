b974dd972be35aaecc69149534b0a5065c749168
souravmondalshuvo <191050332+souravmondalshuvo@users.noreply.github.com>
2026-05-15 06:54:29 +0600

    Lab7Tester creation successful

--- CSE220 Spring 2026 Lab Assignments 7/Lab7Tester.java
+++ CSE220 Spring 2026 Lab Assignments 7/Lab7Tester.java
@@ -1,48 +1,142 @@
-import java.util.ArrayList;
-
 public class Lab7Tester {
-    
+
+    public static void printAnswer(String taskName, int[] answer, String valueName) {
+        System.out.println(taskName + ": Vertex = " + (answer[0] + 1) + ", " + valueName + " = " + answer[1]);
+    }
+
     public static void main(String[] args) {
-    // Task 0
-    int[][] adjMatrix = buildAdjMatrix(N, edgesUndirected, false);
-    ArrayList<ArrayList<int[]>> adjList = buildAdjList(N, edgesUndirected, false);
-    System.out.println("=== Task 0a: Adjacency Matrix ===");
-    printMatrix(adjMatrix);
-    System.out.println("\n=== Task 0b: Adjacency List ===");
-    printAdjList(adjList);
+        int n = 7;
 
-    // Task 1
-    int[] r;
-    r = maxDegreeMatrix(adjMatrix);
-    System.out.println("\nTask 1a -> Vertex " + r[0] + " has max degree = " + r[1]);
-    r = maxDegreeList(adjList);
-    System.out.println("Task 1b -> Vertex " + r[0] + " has max degree = " + r[1]);
+        int[][] undirectedWeightedMatrix = new int[n][n];
+        EdgeNode[] undirectedWeightedList = new EdgeNode[n];
 
-    // Task 2
-    r = maxWeightSumMatrix(adjMatrix);
-    System.out.println("\nTask 2a -> Vertex " + r[0] + " has max weight-sum = " + r[1]);
-    r = maxWeightSumList(adjList);
-    System.out.println("Task 2b -> Vertex " + r[0] + " has max weight-sum = " + r[1]);
+        int[][] undirectedUnweightedMatrix = new int[n][n];
+        EdgeNode[] undirectedUnweightedList = new EdgeNode[n];
 
-    // Task 3
-    int[][] adjMatrixDir = buildAdjMatrix(N, edgesDirected, true);
-    ArrayList<ArrayList<int[]>> adjListDir = buildAdjList(N, edgesDirected, true);
+        int[][] directedWeightedMatrix = new int[n][n];
+        EdgeNode[] directedWeightedList = new EdgeNode[n];
 
-    r = maxOutDegreeMatrix(adjMatrixDir);
-    System.out.println("\nTask 3a -> Max out-degree: vertex " + r[0] + ", degree = " + r[1]);
-    r = maxOutWeightSumMatrix(adjMatrixDir);
-    System.out.println("Task 3a -> Max out-weight-sum: vertex " + r[0] + ", sum = " + r[1]);
-    r = maxOutDegreeList(adjListDir);
-    System.out.println("Task 3b -> Max out-degree: vertex " + r[0] + ", degree = " + r[1]);
-    r = maxOutWeightSumList(adjListDir);
-    System.out.println("Task 3b -> Max out-weight-sum: vertex " + r[0] + ", sum = " + r[1]);
+        // ===================== UNDIRECTED GRAPH =====================
+        // Format: {from, to, weight}
+        // Vertices are written as 0 to 6 internally.
+        // When printed, they will be shown as 1 to 7.
 
-    // Task 4
-    int[][] undirMatrix = directedToUndirectedMatrix(adjMatrixDir);
-    System.out.println("\n=== Task 4a: Directed -> Undirected (Matrix) ===");
-    printMatrix(undirMatrix);
-    ArrayList<ArrayList<int[]>> undirList = directedToUndirectedList(adjListDir);
-    System.out.println("\n=== Task 4b: Directed -> Undirected (List) ===");
-    printAdjList(undirList);
-}
+        int[][] undirectedEdges = {
+            {0, 1, 4},
+            {0, 2, 3},
+            {0, 3, 2},
+            {1, 2, 5},
+            {1, 4, 6},
+            {2, 3, 7},
+            {2, 5, 4},
+            {3, 5, 1},
+            {3, 6, 8},
+            {4, 5, 2},
+            {4, 6, 3},
+            {5, 6, 6}
+        };
+
+        for (int i = 0; i < undirectedEdges.length; i++) {
+            int u = undirectedEdges[i][0];
+            int v = undirectedEdges[i][1];
+            int w = undirectedEdges[i][2];
+
+            // For weighted graph
+            Task0.addUndirectedMatrixEdge(undirectedWeightedMatrix, u, v, w);
+            Task0.addUndirectedListEdge(undirectedWeightedList, u, v, w);
+
+            // For unweighted graph
+            Task0.addUndirectedMatrixEdge(undirectedUnweightedMatrix, u, v, 1);
+            Task0.addUndirectedListEdge(undirectedUnweightedList, u, v, 1);
+        }
+
+        // ===================== DIRECTED GRAPH =====================
+        // Format: {from, to, weight}
+
+        int[][] directedEdges = {
+            {0, 1, 4},
+            {0, 2, 3},
+            {0, 3, 2},
+            {1, 2, 5},
+            {1, 4, 6},
+            {2, 3, 7},
+            {2, 5, 4},
+            {3, 5, 1},
+            {3, 6, 8},
+            {4, 5, 2},
+            {5, 4, 3},
+            {6, 3, 5}
+        };
+
+        for (int i = 0; i < directedEdges.length; i++) {
+            int from = directedEdges[i][0];
+            int to = directedEdges[i][1];
+            int w = directedEdges[i][2];
+
+            Task0.addDirectedMatrixEdge(directedWeightedMatrix, from, to, w);
+            Task0.addDirectedListEdge(directedWeightedList, from, to, w);
+        }
+
+        // ===================== TASK 0 =====================
+
+        System.out.println("Task 0a: Adjacency Matrix");
+        Task0.printMatrix(undirectedWeightedMatrix);
+
+        System.out.println("\nTask 0b: Adjacency List");
+        Task0.printList(undirectedWeightedList);
+
+        // ===================== TASK 1 =====================
+
+        System.out.println("\nTask 1a:");
+        printAnswer("Task 1a", Task1.Task1a(undirectedUnweightedMatrix), "degree");
+
+        System.out.println("\nTask 1b:");
+        printAnswer("Task 1b", Task1.Task1b(undirectedUnweightedList), "degree");
+
+        // ===================== TASK 2 =====================
+
+        System.out.println("\nTask 2a:");
+        printAnswer("Task 2a", Task2.Task2a(undirectedWeightedMatrix), "sum of edge weights");
+
+        System.out.println("\nTask 2b:");
+        printAnswer("Task 2b", Task2.Task2b(undirectedWeightedList), "sum of edge weights");
+
+        // ===================== TASK 3 =====================
+
+        System.out.println("\nTask 3a:");
+        printAnswer(
+            "Task 3a Maximum Outgoing Degree",
+            Task3.Task3a_MaxOutgoingDegree(directedWeightedMatrix),
+            "outgoing degree"
+        );
+
+        printAnswer(
+            "Task 3a Maximum Outgoing Weight Sum",
+            Task3.Task3a_MaxOutgoingWeightSum(directedWeightedMatrix),
+            "outgoing weight sum"
+        );
+
+        System.out.println("\nTask 3b:");
+        printAnswer(
+            "Task 3b Maximum Outgoing Degree",
+            Task3.Task3b_MaxOutgoingDegree(directedWeightedList),
+            "outgoing degree"
+        );
+
+        printAnswer(
+            "Task 3b Maximum Outgoing Weight Sum",
+            Task3.Task3b_MaxOutgoingWeightSum(directedWeightedList),
+            "outgoing weight sum"
+        );
+
+        // ===================== TASK 4 =====================
+
+        System.out.println("\nTask 4a: Directed Matrix Converted to Undirected Matrix");
+        int[][] convertedMatrix = Task4.Task4a(directedWeightedMatrix);
+        Task0.printMatrix(convertedMatrix);
+
+        System.out.println("\nTask 4b: Directed List Converted to Undirected List");
+        EdgeNode[] convertedList = Task4.Task4b(directedWeightedList);
+        Task0.printList(convertedList);
+    }
 }
