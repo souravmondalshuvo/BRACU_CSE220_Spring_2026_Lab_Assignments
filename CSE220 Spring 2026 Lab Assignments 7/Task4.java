2a7ca818ca4618cb3b478bde27fff4ca1628442e
souravmondalshuvo <191050332+souravmondalshuvo@users.noreply.github.com>
2026-05-15 06:39:24 +0600

    Task4b of Task4 completed

--- CSE220 Spring 2026 Lab Assignments 7/Task4.java
+++ CSE220 Spring 2026 Lab Assignments 7/Task4.java
@@ -22,4 +22,46 @@ public class Task4 {
 
         return undirectedMatrix;
     }
+
+    // ========================== Task 4b ==========================
+
+    public static void addDirectedListEdge(EdgeNode[] list, int from, int to, int weight) {
+
+        list[from] = new EdgeNode(to, weight, list[from]);
+    }
+
+    public static void addUndirectedListEdge(EdgeNode[] list, int u, int v, int weight) {
+
+        addDirectedListEdge(list, u, v, weight);
+        addDirectedListEdge(list, v, u, weight);
+    }
+
+    public static EdgeNode[] Task4b(EdgeNode[] directedList) {
+
+        int n = directedList.length;
+        int[][] weightMatrix = new int[n][n];
+
+        for(int i = 0; i < n; i++) {
+            EdgeNode current = directedList[i];
+
+            while(current != null) {
+                weightMatrix[i][current.to] += current.weight;
+                current = current.next;
+            }
+        }
+
+        EdgeNode[] undirectedList = new EdgeNode[n];
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                int totalWeight = weightMatrix[i][j] + weightMatrix[j][i];
+
+                if(totalWeight != 0) {
+                    addUndirectedListEdge(undirectedList, i, j, totalWeight);
+                }
+            }
+        }
+
+        return undirectedList;
+    }
 }
